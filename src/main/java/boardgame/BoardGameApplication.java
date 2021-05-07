89302c3bcd692eed323dc335588bea67ff758b0b
Nagy Enikő <nagye99@gmail.com>
2021-05-07 12:16:50 +0200

    A játéktábla létrehozása és a hozzá való Controller és futtatáshoz szükséges osztályok elkészítése

---
+++ src/main/java/boardgame/BoardGameApplication.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BoardGameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/scene1.fxml"));
+        stage.setTitle("Board Game Project with JavaFX");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
---
+++ src/main/java/boardgame/BoardGameController.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import org.tinylog.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardGameController {
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private void initialize() {
+        createBoard();
+    }
+
+    private void createBoard() {
+        for (int i = 0; i < board.getRowCount(); i++) {
+            for (int j = 0; j < board.getColumnCount(); j++) {
+                StackPane square;
+                square = createSquare();
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare() {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        //square.setOnMouseClicked(this::handleMouseClick);
+        return square;
+    }
+
+}
