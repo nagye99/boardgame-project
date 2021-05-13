b2c409a7a960a02414ac25eb917ca025af9f4845
Nagy Enikő <nagye99@gmail.com>
2021-05-14 01:02:31 +0200

    Az egyéni eredményekhez tartozó tábla vezérlése létrehozása.

---
+++ src/main/java/boardgame/controller/ResultPlayerController.java
@@ -0,0 +1,73 @@
+package boardgame.controller;
+
+import boardgame.results.BoardGameHandleResults;
+import boardgame.results.Player;
+import boardgame.results.Results;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ResultPlayerController {
+
+    @FXML
+    private TableView<Player> ResultPlayerTable;
+
+    @FXML
+    private TableColumn<Player, String> name;
+
+    @FXML
+    private TableColumn<Player, Integer> plays;
+
+    @FXML
+    private TableColumn<Player, Integer> wins;
+
+    @FXML
+    private void initialize() throws IOException {
+        List<Player> playerList = BoardGameHandleResults.GetPlayer();
+
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        plays.setCellValueFactory(new PropertyValueFactory<>("plays"));
+        wins.setCellValueFactory(new PropertyValueFactory<>("wins"));
+
+        ObservableList<Player> observablePlayer = FXCollections.observableArrayList();
+        observablePlayer.addAll(playerList);
+
+        ResultPlayerTable.setItems(observablePlayer);
+    }
+
+    public void handleRestartButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ResultPlayerController.class.getResource("/fxml/openPage.fxml"));
+        Parent root = fxmlLoader.load();
+        OpenPageController controller = fxmlLoader.<OpenPageController>getController();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void handleResultButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ResultPlayerController.class.getResource("/fxml/result.fxml"));
+        Parent root = fxmlLoader.load();
+        ResultController controller = fxmlLoader.<ResultController>getController();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void handleExitButton(ActionEvent event) throws IOException {
+        Platform.exit();
+    }
+
+}
