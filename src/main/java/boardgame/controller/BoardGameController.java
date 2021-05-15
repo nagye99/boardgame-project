1d413858302c5aeaa191bf905e3053d1b713315a
Nagy Enikő <nagye99@gmail.com>
2021-05-15 18:58:21 +0200

    A borderPane css osztálya átnevezve

--- src/main/java/boardgame/controller/BoardGameController.java
+++ src/main/java/boardgame/controller/BoardGameController.java
@@ -15,7 +15,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -29,11 +28,11 @@ import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-import static boardgame.model.BoardGameModel.getDirection;
 
 public class BoardGameController {
 
@@ -51,23 +50,15 @@ public class BoardGameController {
     }
 
     private SelectionPhase selectionPhase = SelectionPhase.SELECT_FROM;
-
     private ObjectProperty<NextPlayer> nextPlayer = new SimpleObjectProperty<>();
-
-    private  List<Position> selectablePositions = new ArrayList<>();
-
+    private List<Position> selectablePositions = new ArrayList<>();
     private Position selected;
-
     private BoardGameModel model;
-
     private Timeline stopWatchTimeline;
     private Instant startTime;
-
     private IntegerProperty steps = new SimpleIntegerProperty(0);
-
     private StringProperty redName = new SimpleStringProperty();
     private StringProperty blueName = new SimpleStringProperty();
-
 
 
     @FXML
@@ -89,7 +80,7 @@ public class BoardGameController {
     @FXML
     private void initialize() {
         model = new BoardGameModel();
-        Bindings.bindBidirectional(nextPlayer, BoardGameModel.nextPlayer);
+        Bindings.bindBidirectional(nextPlayer, model.nextPlayer);
         model.nextPlayerProperty().addListener(this::nextPlayerChange);
         createBoard();
         createPieces();
@@ -97,7 +88,6 @@ public class BoardGameController {
         setSelectablePositions();
         showSelectablePositions();
         writeOutName();
-        //observeNexPlayerChange();
         createStopWatch();
         //Logger.error(steps);
         stepsLabel.textProperty().bind(steps.asString());
@@ -109,7 +99,7 @@ public class BoardGameController {
         for (int i = 0; i < board.getRowCount(); i++) {
             for (int j = 0; j < board.getColumnCount(); j++) {
                 StackPane square;
-                    square = createSquare();
+                square = createSquare();
                 board.add(square, j, i);
             }
         }
@@ -136,8 +126,8 @@ public class BoardGameController {
         return piece;
     }
 
-    private void createBlocks(){
-        for(int i =0; i < model.getBlockCount(); i++){
+    private void createBlocks() {
+        for (int i = 0; i < model.getBlockCount(); i++) {
             getSquare(model.getBlockPosition(i)).getStyleClass().remove("square");
             getSquare(model.getBlockPosition(i)).getStyleClass().add("denied");
         }
@@ -148,7 +138,7 @@ public class BoardGameController {
         this.blueName.set(player2);
     }
 
-    private void writeOutName(){
+    private void writeOutName() {
         redPlayerLabel.textProperty().bind(redName);
         bluePlayerLabel.textProperty().bind(blueName);
     }
@@ -163,76 +153,43 @@ public class BoardGameController {
         handleClickOnSquare(position);
     }
 
-    private void nextPlayerChange(ObservableValue<? extends NextPlayer> observable, NextPlayer oldPlayer, NextPlayer newPlayer ) {
-        if(newPlayer == NextPlayer.RED_PLAYER){
+    private void nextPlayerChange(ObservableValue<? extends NextPlayer> observable, NextPlayer oldPlayer, NextPlayer newPlayer) {
+        if (newPlayer == NextPlayer.RED_PLAYER) {
             nextPlayerLabel.setText("A piros játékos következik");
             nextPlayerLabel.setTextFill(Color.RED);
-        }
-        else if (newPlayer == NextPlayer.BLUE_PLAYER){
+        } else if (newPlayer == NextPlayer.BLUE_PLAYER) {
             nextPlayerLabel.setText("A kék játékos következik");
             nextPlayerLabel.setTextFill(Color.BLUE);
         }
     }
 
-    private void handleClickOnSquare(Position position){
-        Logger.debug("ClickSquare "+ nextPlayer + selectionPhase);
-        /*switch (nextPlayer.get()){
-            case RED_PLAYER -> {*/
-                switch (selectionPhase) {
-                    case SELECT_FROM -> {
-                        if (selectablePositions.contains(position)) {
-                            selectPosition(position);
-                            alterSelectionPhase();
-                        }
-                    }
-                    case SELECT_TO -> {
-                        if(selected.equals(position)){
-                            deselectSelectedPosition();
-                            alterSelectionPhase();
-                        }
-                        else if (selectablePositions.contains(position)) {
-                            var pieceNumber = model.getPieceNumber(selected).getAsInt();
-                            var direction = getDirection(selected,position);
-                            Logger.debug("Moving piece {} {}", pieceNumber, direction);
-                            model.move(pieceNumber, direction);
-                            steps.set(steps.get() + 1);
-                            deselectSelectedPosition();
-                            if (!model.canBlueMove()){
-                                //endGame("piros", redName.get(), blueName.get());
-                            }
-                            alterSelectionPhase();
-                        }
-                    }
-                }
-           /* }
-            case BLUE_PLAYER -> {
-                switch (selectionPhase) {
-                    case SELECT_FROM -> {
-                        if (selectablePositions.contains(position)) {
-                            selectPosition(position);
-                            alterSelectionPhase();
-                        }
-                    }
-                    case SELECT_TO -> {
-                        if(selected.equals(position)){
-                            deselectSelectedPosition();
-                            alterSelectionPhase();
-                        }else if (selectablePositions.contains(position)) {
-                            var pieceNumber = model.getPieceNumber(selected).getAsInt();
-                            var direction = getDirection(selected,position);
-                            Logger.debug("Moving piece {} {}", pieceNumber, direction);
-                            model.move(pieceNumber, direction);
-                            steps.set(steps.get() + 1);
-                            deselectSelectedPosition();
-                            if (!model.canRedMove()){
-                                endGame("piros", blueName.get(), redName.get());
-                            }
-                            alterSelectionPhase();
-                        }
-                    }
+    private void handleClickOnSquare(Position position) {
+        Logger.debug("ClickSquare " + nextPlayer + selectionPhase);
+        switch (selectionPhase) {
+            case SELECT_FROM -> {
+                if (selectablePositions.contains(position)) {
+                    selectPosition(position);
+                    alterSelectionPhase();
                 }
             }
-        }*/
+            case SELECT_TO -> {
+                if (selected.equals(position)) {
+                    deselectSelectedPosition();
+                    alterSelectionPhase();
+                } else if (selectablePositions.contains(position)) {
+                    var pieceNumber = model.getPieceNumber(selected).getAsInt();
+                    var direction = model.getDirection(selected, position);
+                    Logger.debug("Moving piece {} {}", pieceNumber, direction);
+                    model.move(pieceNumber, direction);
+                    steps.set(steps.get() + 1);
+                    deselectSelectedPosition();
+                    if (!model.canBlueMove() && nextPlayer.get() == NextPlayer.BLUE_PLAYER || !model.canRedMove() && nextPlayer.get() == NextPlayer.RED_PLAYER) {
+                        endGame(nextPlayer.get());
+                    }
+                    alterSelectionPhase();
+                }
+            }
+        }
     }
 
     private void alterSelectionPhase() {
@@ -274,8 +231,8 @@ public class BoardGameController {
     private void setSelectablePositions() {
         selectablePositions.clear();
         Logger.debug("selectablePosition" + nextPlayer);
-        switch (nextPlayer.get()){
-            case RED_PLAYER ->{
+        switch (nextPlayer.get()) {
+            case RED_PLAYER -> {
                 switch (selectionPhase) {
                     case SELECT_FROM -> selectablePositions.addAll(model.getRedPositions());
                     case SELECT_TO -> {
@@ -321,18 +278,18 @@ public class BoardGameController {
     }
 
     @FXML
-    private void changeScene(String color, String winnerName, String loserName){
+    private void changeScene(String color, String winnerName, String loserName) {
         try {
-        FXMLLoader fxmlLoader = new FXMLLoader(OpenPageController.class.getResource("/fxml/winner.fxml"));
-        Logger.warn(BoardGameController.class);
-        //Logger.error(BoardGameController.class.getResource("/fxml/winner.fxml"));
-        Parent root = fxmlLoader.load();
-        WinnerController controller = fxmlLoader.<WinnerController>getController();
-        controller.setWinner(color, winnerName, loserName);
-        Stage stage = (Stage) ((Node) board).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();}
-        catch (IOException e) {
+            FXMLLoader fxmlLoader = new FXMLLoader(OpenPageController.class.getResource("/fxml/winner.fxml"));
+            Logger.warn(BoardGameController.class);
+            //Logger.error(BoardGameController.class.getResource("/fxml/winner.fxml"));
+            Parent root = fxmlLoader.load();
+            WinnerController controller = fxmlLoader.<WinnerController>getController();
+            controller.setWinner(color, winnerName, loserName);
+            Stage stage = (Stage) ((Node) board).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
             System.out.println(e);
             e.getCause();
             e.getMessage();
@@ -369,9 +326,25 @@ public class BoardGameController {
         stage.show();
     }
 
-    private void endGame(String color, String winner, String loser){
+    private void endGame(NextPlayer nextPlayer) {
         stopWatchTimeline.stop();
-        Results result = new Results(redName.get(), blueName.get(), winner, steps.get(), DurationFormatUtils.formatDuration(startTime.until(Instant.now(), ChronoUnit.MILLIS), "HH:mm:ss"));
+        String winner;
+        String loser;
+        String color;
+        switch (nextPlayer) {
+            case RED_PLAYER -> {
+                winner = blueName.get();
+                loser = redName.get();
+                color = "kék";
+            }
+            case BLUE_PLAYER -> {
+                winner = redName.get();
+                loser = blueName.get();
+                color = "piros";
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + nextPlayer);
+        }
+        Results result = new Results(redName.get(), blueName.get(), winner, steps.get(), DurationFormatUtils.formatDuration(startTime.until(Instant.now(), ChronoUnit.MILLIS), "HH:mm:ss"), LocalDateTime.now());
         BoardGameHandleResults.SaveResult(result);
         BoardGameHandleResults.SaveWinnerPlayer(winner);
         BoardGameHandleResults.SaveLoserPlayer(loser);
