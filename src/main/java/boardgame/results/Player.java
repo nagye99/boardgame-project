1cc794954fb91e8dd915b626c67409d726a34ed4
Nagy Enikő <nagye99@gmail.com>
2021-05-17 10:21:29 +0200

    Javadoc megjegyzések hozzáadva

--- src/main/java/boardgame/results/Player.java
+++ src/main/java/boardgame/results/Player.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Class represents the players and there stats.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
