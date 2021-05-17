ac41fab62ae398800e95c449f699bd22cdae8945
Nagy Enikő <nagye99@gmail.com>
2021-05-17 10:11:57 +0200

    Javadoc megjegyzések hozzáadva. Main metódus eltávolítva

--- src/main/java/boardgame/model/Position.java
+++ src/main/java/boardgame/model/Position.java
@@ -2,8 +2,20 @@ package boardgame.model;
 
 import java.util.Objects;
 
+/**
+ * Class represents the positions of the gametable.
+ *
+ * @param row gives the row number of position
+ * @param col gives the column number of position
+ */
 public record Position(int row, int col) {
 
+    /**
+     * This method calculate the position after a step of the given position.
+     *
+     * @param direction that represent thet direction of move
+     * @return the new position after the step
+     */
     public Position moveTo(Direction direction) {
         return new Position(row + direction.getRowChange(), col + direction.getColChange());
     }
@@ -25,10 +37,4 @@ public record Position(int row, int col) {
     public String toString() {
         return String.format("(%d,%d)", row, col);
     }
-
-    public static void main(String[] args) {
-        Position pos = new Position(1,1);
-        System.out.println(pos.equals(new Position(1,1)));
-    }
-
 }
