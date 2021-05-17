69779640c76ad780545656f325e8fc7691343907
Nagy Enikő <nagye99@gmail.com>
2021-05-17 10:08:52 +0200

    Javadoc megjegyzések hozzáadva

--- src/main/java/boardgame/model/Direction.java
+++ src/main/java/boardgame/model/Direction.java
@@ -1,8 +1,21 @@
 package boardgame.model;
 
+/**
+ * This interface join the different Direction representer classes.
+ */
 public interface Direction {
 
+    /**
+     * This method represents the change of the row number.
+     *
+     * @return the difference between old and new row number
+     */
     int getRowChange();
+    /**
+     * This method represents the change of the column number.
+     *
+     * @return the difference between old and new column number
+     */
     int getColChange();
 
 }
