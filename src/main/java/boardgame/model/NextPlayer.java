b03e41351a5a35f005e8c67d60e7864c669eaadd
Nagy Enikő <nagye99@gmail.com>
2021-05-17 10:09:29 +0200

    Javadoc megjegyzések hozzáadva

--- src/main/java/boardgame/model/NextPlayer.java
+++ src/main/java/boardgame/model/NextPlayer.java
@@ -1,9 +1,23 @@
 package boardgame.model;
 
+/**
+ * This class represents the players.
+ */
 public enum NextPlayer {
+    /**
+     * The red player.
+     */
     RED_PLAYER,
+    /**
+     * The blue player.
+     */
     BLUE_PLAYER;
 
+    /**
+     * This method change the actual player to the other player.
+     *
+     * @return the next player
+     */
     public NextPlayer alter() {
         return switch (this) {
             case RED_PLAYER -> BLUE_PLAYER;
