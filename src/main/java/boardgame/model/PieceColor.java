31e1760deda58549475841e9dbcef686dda0950f
Nagy Enikő <nagye99@gmail.com>
2021-05-17 10:11:06 +0200

    Javadoc megjegyzések hozzáadva

--- src/main/java/boardgame/model/PieceColor.java
+++ src/main/java/boardgame/model/PieceColor.java
@@ -1,6 +1,15 @@
 package boardgame.model;
 
+/**
+ * This enum class gives the possible colours of pieces.
+ */
 public enum PieceColor {
+    /**
+     * The colour red.
+     */
     RED,
+    /**
+     * THe colour blue.
+     */
     BLUE
 }
